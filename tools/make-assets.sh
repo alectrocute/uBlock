3738ed8a694293da6c8a63f208d244910cd3c0d2
Alec Armbruster <alecsamuelarmbruster@gmail.com>
2019-11-20 18:58:53 -0800

    Add files via upload

---
+++ tools/make-assets.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#
+# This script assumes a linux environment
+
+DES=$1/assets
+
+printf "*** Packaging assets in $DES... "
+
+if [ -n "${TRAVIS_TAG}" ]; then
+  pushd .. > /dev/null
+  git clone --depth 1 https://github.com/uBlockOrigin/uAssets.git
+  popd > /dev/null
+fi
+
+rm -rf $DES
+cp -R ./assets $DES/
+
+if [ -f ./tmp/requests.json.gz ]; then
+    gunzip -c ./tmp/requests.json.gz > $DES/requests.json
+fi
+
+mkdir $DES/thirdparties
+cp -R ../uAssets/thirdparties/easylist-downloads.adblockplus.org $DES/thirdparties/
+cp -R ../uAssets/thirdparties/mirror1.malwaredomains.com         $DES/thirdparties/
+cp -R ../uAssets/thirdparties/pgl.yoyo.org                       $DES/thirdparties/
+cp -R ../uAssets/thirdparties/publicsuffix.org                   $DES/thirdparties/
+cp -R ../uAssets/thirdparties/www.malwaredomainlist.com          $DES/thirdparties/
+
+mkdir $DES/ublock
+cp -R ../uAssets/filters/*                                       $DES/ublock/
+# Optional filter lists: do not include in package
+rm    $DES/ublock/annoyances.txt
+rm    $DES/ublock/resources.txt
+
+echo "done."
